119f2a88c788d6e359074981370eff44e13e67f0
teshaTe <tereshin.tesha@gmail.com>
2023-08-30 14:36:40 +0100

    ADDED: the google test for canvas class

---
+++ tests/canvas_tests.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include "RenderingCanvas.hpp"
+
+using namespace ray_tracer;
+
+TEST(CreateCanvasTest, TestingCanvasAPI)
+{
+}
+
+TEST(SavingCanvasTest, TestingCanvasSavingToFile)
+{
+}
+
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
