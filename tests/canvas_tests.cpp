9733799bc8ff10d50a4b4c878ac8d2b97ec856f1
TeSha <tereshin.tesha@gmail.com>
2023-09-13 13:34:18 +0100

    ADDED: finished test for the canvas class

--- tests/canvas_tests.cpp
+++ tests/canvas_tests.cpp
@@ -29,7 +29,22 @@ TEST(CreateCanvasTest, TestingCanvasAPI)
 
 TEST(SavingCanvasTest, TestingCanvasSavingToFile)
 {
-    Canvas<int> canvas(5, 3);
+    Canvas<int> canvas(3, 3);
+    Color<int> c1{255, 0, 0};
+    Color<int> c2{0, 255, 0};
+    Color<int> c3{0, 0, 255};
+    canvas.set_pixel(0, 0, c1);
+    canvas.set_pixel(1, 1, c2);
+    canvas.set_pixel(2, 2, c3);
+
+    std::string file_name = "test_img";
+    std::string path = "..";
+    canvas.save_to_ppm(file_name, path);
+
+    Canvas<int> canvas_dst(3, 3);
+    canvas_dst = canvas.load_ppm(file_name, path);
+
+    ASSERT_EQ(canvas, canvas_dst);
 }
 
 
