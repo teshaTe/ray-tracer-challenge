cd22437324c5a235320733726a5b4e1363602f79
TeSha <tereshin.tesha@gmail.com>
2023-09-13 13:33:56 +0100

    ADDED: sample for testing the functionality

--- Samples/projectile_gen_sample.cpp
+++ Samples/projectile_gen_sample.cpp
@@ -22,7 +22,7 @@ struct Projectile
 
 struct Environment
 {
-    Vector3<float> m_gravity{0, -9.8, 0};
+    Vector3<float> m_gravity{0, 0, 0};
     Vector3<float> m_wind{0, 0, 0};
     Environment() {}
     Environment(Vector3<float> gravity, Vector3<float> wind): m_gravity(gravity), m_wind(wind) {}
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
         std::cout << "Position: " << p0.m_position << "; Velocity: " << p0.m_velocity <<
                      "; Gravity: " << e.m_gravity << "; Wind: " << e.m_wind << std::endl;
         p0 = tick(p0, e);
-        image.set_pixel(p0.m_position.x(), p0.m_position.y(), c);
+        image.set_pixel(p0.m_position.x(), image.get_canvas_height() - p0.m_position.y(), c);
         if(i == max_iterations)
             break;
         else
@@ -63,7 +63,14 @@ int main(int argc, char *argv[])
     }
     std::string file_name = "projectile";
     std::string path = "..";
+
+    Canvas<int> dst{900, 555};
     image.save_to_ppm(file_name, path);
+    dst = image.load_ppm(file_name, path);
+
+    if(dst == image)
+        std::cout << "Computed and saved images are the same!" << std::endl;
+
 
     return 0;
 }
