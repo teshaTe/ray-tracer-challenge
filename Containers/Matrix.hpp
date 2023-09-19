26f384880b7d1483126afa1e12e1dfb543c8afa2
TeSha <tereshin.tesha@gmail.com>
2023-09-19 22:29:44 +0100

    ADDED: transpose & multtiplication by vector ops

--- Containers/Matrix.hpp
+++ Containers/Matrix.hpp
@@ -4,8 +4,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include "Vector.hpp"
+
 
 namespace ray_tracer {
+
+/**
+ * @brief The Matrix class
+ */
 
 template<class T>
 class Matrix
@@ -14,18 +20,25 @@ private:
     std::vector<std::vector<T>> m_mat;
     int m_rows;
     int m_cols;
+    T m_zero = 0;
 
     typedef typename std::vector<std::vector<T>>::iterator iterator;
     typedef typename std::vector<std::vector<T>>::const_iterator const_iterator;
 
 public:
+    /**
+     * @brief Matrix
+     * @param rows
+     * @param cols
+     * @param data
+     */
     Matrix(const int rows, const int cols, const std::vector<std::vector<T>> *data = nullptr): m_rows{rows}, m_cols{cols}
     {
         m_mat.resize(rows);
         for(auto &c : m_mat)
         {
             c.resize(cols);
-            std::fill(c.begin(), c.end(), 0);
+            std::fill(c.begin(), c.end(), m_zero);
         }
 
         if(data != nullptr)
@@ -33,16 +46,49 @@ public:
                 m_mat = *data;
     }
 
+    /**
+     * @brief Matrix
+     * @param other
+     */
     Matrix(const Matrix<T>& other) { this->m_mat = other.m_mat; m_rows = other.m_rows; m_cols = other.m_cols; }
 
+    /**
+     * @brief begin
+     * @return
+     */
     iterator begin() { return m_mat.begin(); }
+
+    /**
+     * @brief begin
+     * @return
+     */
     const_iterator begin() const { return m_mat.begin(); }
 
+    /**
+     * @brief end
+     * @return
+     */
     iterator end() { return m_mat.end(); }
+
+    /**
+     * @brief end
+     * @return
+     */
     const_iterator end() const { return m_mat.end(); }
 
+    /**
+     * @brief operator ()
+     * @param i
+     * @param j
+     * @return
+     */
     T& operator()(int i, int j) { return m_mat[i][j]; }
 
+    /**
+     * @brief operator +
+     * @param other
+     * @return
+     */
     Matrix<T> operator+(const Matrix<T>& other)
     {
         if(this->m_cols == other.m_cols && this->m_rows == other.m_rows)
@@ -54,6 +100,12 @@ public:
         else
             throw("Sizes of two matrices are mismatched");
     }
+
+    /**
+     * @brief operator -
+     * @param other
+     * @return
+     */
     Matrix<T> operator-(const Matrix<T>& other)
     {
         if(this->m_cols == other.m_cols && this->m_rows == other.m_rows)
@@ -65,6 +117,12 @@ public:
         else
             throw("Sizes of two matrices are mismatched");
     }
+
+    /**
+     * @brief operator /
+     * @param scalar
+     * @return
+     */
     Matrix<T> operator/(const T scalar)
     {
         for(size_t r = 0; r < m_mat.size(); ++r)
@@ -72,6 +130,11 @@ public:
         return Matrix<T>(m_rows, m_cols, &m_mat);
     }
 
+    /**
+     * @brief operator *
+     * @param scalar
+     * @return
+     */
     Matrix<T> operator*(const T scalar)
     {
         for(size_t r = 0; r < m_mat.size(); ++r)
@@ -79,6 +142,11 @@ public:
         return Matrix<T>(m_rows, m_cols, &m_mat);
     }
 
+    /**
+     * @brief operator ==
+     * @param other
+     * @return
+     */
     bool operator==(const Matrix<T>& other) const
     {
         if(this->m_cols == other.m_cols && this->m_rows == other.m_rows)
@@ -98,6 +166,12 @@ public:
             throw("Sizes of two matrices are mismatched");
     }
 
+    /**
+     * @brief operator <<
+     * @param os
+     * @param m
+     * @return
+     */
     friend std::ostream& operator<<(std::ostream& os, Matrix<T>& m)
     {
         for(int i = 0; i < m.m_rows; ++i)
@@ -109,6 +183,11 @@ public:
         return os;
     }
 
+    /**
+     * @brief mul
+     * @param mat
+     * @return
+     */
     Matrix<T> mul(const Matrix<T>& mat)
     {
         if(m_cols == mat.m_rows)
@@ -125,31 +204,91 @@ public:
             throw("Matrix dimesnions misalignment was detected. m1.cols != m2.rows!");
     }
 
+    /**
+     * @brief mul
+     * @param vec
+     * @return
+     */
+    Matrix<T> mul(const Vector<T>& vec)
+    {
+        std::vector<std::vector<T>> vec_d;
+        vec_d.reserve(1);
+        vec_d.push_back(vec.data());
+        Matrix<T> v_col{static_cast<int>(vec.size()), 1, &vec_d};
+        return mul(v_col);
+    }
+
+    /**
+     * @brief I
+     */
     void I()
     {
         for(size_t r=0; r < m_rows; ++r)
             m_mat[r][r] = 1;
     }
+
+    /**
+     * @brief tr
+     * @return
+     */
     Matrix<T> tr()
     {
+        std::vector<std::vector<T>> tmp;
+        tmp.resize(m_cols);
 
+        for(int r = 0; r < m_cols; ++r)
+        {
+            tmp[r].resize(m_rows);
+            for(int c=0; c < m_rows; ++c)
+                tmp[r][c] = m_mat[c][r];
+        }
+        return Matrix<T>{m_cols, m_rows, &tmp};
     }
+
+    /**
+     * @brief inv
+     * @return
+     */
     Matrix<T> inv()
     {
 
     }
+
+    /**
+     * @brief det
+     * @return
+     */
     Matrix<T> det()
     {
 
     }
+
+    /**
+     * @brief compute_minor
+     * @return
+     */
     Matrix<T> compute_minor()
     {
 
     }
+
+    /**
+     * @brief compute_cofactor
+     * @return
+     */
     Matrix<T> compute_cofactor()
     {
 
     }
+
+    /**
+     * @brief block
+     * @param width
+     * @param height
+     * @param row
+     * @param col
+     * @return
+     */
     Matrix<T> block(const int width, const int height, const int row, const int col)
     {
         Matrix<T> tmp{width, height};
@@ -160,6 +299,12 @@ public:
 
         return tmp;
     }
+
+    /**
+     * @brief data
+     * @return
+     */
+    std::vector<std::vector<T>> data() const { return m_mat; }
 
     ~Matrix() = default;
 };
