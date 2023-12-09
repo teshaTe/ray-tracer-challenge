07b279c07ff357a33dd094eb48b27b47cf10e3ea
TeSha <tereshin.tesha@gmail.com>
2023-12-09 20:19:25 +0000

    [UODATED] added function for computing transforms

--- RayTracer/Core/shape.h
+++ RayTracer/Core/shape.h
@@ -18,14 +18,18 @@ public:
     virtual Vector<float> get_origin() const = 0;
     virtual std::vector<types::intersection> intersect(const Ray &ray) const = 0;
 
-    //    virtual void transform(Vector<float> transform) = 0;
-    //    virtual void scale() = 0;
+    virtual void transform(const Matrix<float> &transform) = 0;
+    virtual void transform(const Vector<float> &tr_vec, const Matrix<float> &rot_mat) = 0;
+    virtual Matrix<float> get_transform() = 0;
+    virtual Matrix<float> get_rotation_matrix() = 0;
+    virtual Vector<float> get_translation() = 0;
+
+    virtual void scale(const Vector<float> &scale_vec) = 0;
 
     virtual int get_id() const = 0;
     virtual std::string get_name() const = 0;
 
     bool get_surface_hit(const std::vector<types::intersection> &intersections, float *res);
-
 };
 
 } // namespace ray_tracer::prims
