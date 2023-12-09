c5c4548ecf7911c69af56d2d21fe214976e0d2b7
TeSha <tereshin.tesha@gmail.com>
2023-12-09 20:16:55 +0000

    [UPDATED] added transformation for the ray

--- RayTracer/Core/Ray.hpp
+++ RayTracer/Core/Ray.hpp
@@ -2,6 +2,7 @@
 #define RAY_HPP
 
 #include "Containers/Vector.hpp"
+#include "MatrixUtils.hpp"
 
 namespace ray_tracer {
 
@@ -26,6 +27,19 @@ public:
     Vector<float> position(const Ray &ray, const float t)
     {
         return ray.m_origin + ray.m_direction * t;
+    }
+
+    Ray translate(const Vector<float> &translation)
+    {
+        Vector<float> new_origin = m_origin + translation;
+        return Ray(new_origin, m_direction);
+    }
+
+    Ray scale(const Vector<float> &scaling_vec)
+    {
+        Vector<float> new_origin = Vector<float>{m_origin[0]*scaling_vec[0], m_origin[1]*scaling_vec[1], m_origin[2]*scaling_vec[2]};
+        Vector<float> new_direction = Vector<float>{m_direction[0]*scaling_vec[0], m_direction[1]*scaling_vec[1], m_direction[2]*scaling_vec[2]};
+        return Ray(new_origin, new_direction);
     }
 
     const Vector<float> get_origin() const { return m_origin; }
