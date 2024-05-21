7246a31b31149b7eb125ad8431821fb726715712
tesha <tereshin.tesha@gmail.com>
2024-05-21 20:45:47 +0100

    [ADDED] light class (empty)

---
+++ RayTracer/Core/Light.hpp
@@ -0,0 +1,21 @@
+#ifndef LIGHT_HPP
+#define LIGHT_HPP
+
+#include "Containers/Vector.hpp"
+#include "MatrixUtils.hpp"
+
+
+namespace ray_tracer {
+
+class Light
+{
+private:
+
+
+public:
+
+};
+
+} //namespace ray_tracer
+
+#endif // LIGHT_HPP
