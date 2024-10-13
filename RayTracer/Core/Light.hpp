1b753009ea225503d4f645b9e3bd04ff4684ffef
teshaTe <tereshin.tesha@gmail.com>
2024-10-13 21:31:12 +0100

    [ADDED] abstract class for the light object

--- RayTracer/Core/Light.hpp
+++ RayTracer/Core/Light.hpp
@@ -2,17 +2,22 @@
 #define LIGHT_HPP
 
 #include "Containers/Vector.hpp"
+#include "Containers/Color.hpp"
 #include "MatrixUtils.hpp"
 
 
-namespace ray_tracer {
+namespace ray_tracer::lights {
 
 class Light
 {
-private:
-
-
 public:
+    virtual ~Light() = default;
+
+    virtual Vector<float> get_origin() const = 0;
+    virtual Color<float> get_intensity() const = 0;
+
+    virtual void set_origin(const Vector<float> &origin) = 0;
+    virtual void set_intensity(const Color<float> &intensity) = 0;
 
 };
 
