811605df73e6a12cc0865e94509a9c3134c9471f
teshaTe <tereshin.tesha@gmail.com>
2024-11-24 18:39:36 +0000

    [UPDATED] material class replaced with struct for now
    [UPDATED] material class -> material struct for now

--- RayTracer/Core/Material.hpp
+++ RayTracer/Core/Material.hpp
@@ -1,23 +1,53 @@
 #ifndef MATERIAL_HPP
 #define MATERIAL_HPP
 
-#include "Containers/Vector.hpp"
-#include "MatrixUtils.hpp"
+#include "Containers/Color.hpp"
 
-namespace ray_tracer {
+
+namespace ray_tracer::materials {
 
 /**
- * @brief The Ray class
+ * @brief The Material class
  */
-class Material
+struct BaseMaterial
 {
-private:
+    Color<float> color{0, 1, 0};
+    float ambient = 0;
+    float diffuse = 0;
+    float specular = 0;
+    float snininess = 0;
 
-
-public:
-
+    bool operator==(const BaseMaterial &other) const
+    {
+        if(color == other.color)
+        {
+            if(ambient == other.ambient)
+            {
+                if(diffuse == other.diffuse)
+                {
+                    if (specular == other.specular)
+                    {
+                        if(snininess==other.snininess)
+                        {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 };
 
-} //namespace ray_tracer
+} //namespace ray_tracer::materials
 
 #endif // MATERIAL_HPP
