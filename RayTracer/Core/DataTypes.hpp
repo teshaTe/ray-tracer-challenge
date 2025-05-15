c68be040de8809c44b782ad2d3d4e278ca8cfe23
teshaTe <tereshin.tesha@gmail.com>
2025-05-15 21:40:47 +0100

    added comparison overload operator for intersection obj

--- RayTracer/Core/DataTypes.hpp
+++ RayTracer/Core/DataTypes.hpp
@@ -10,6 +10,11 @@ struct intersection
 {
     std::string obj_type;
     float t;
+
+    bool operator<(const intersection& obj) const
+    {
+        return t < obj.t;
+    }
 };
 
 } // namespace ray_tracer::types
