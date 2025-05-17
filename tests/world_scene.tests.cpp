e561cc5d81b1d85919652e4ea7035cc2c2f9f59a
teshaTe <tereshin.tesha@gmail.com>
2025-05-17 22:28:31 +0100

    added a new test, removed extra output

--- tests/world_scene.tests.cpp
+++ tests/world_scene.tests.cpp
@@ -60,27 +60,42 @@ TEST(WorldSceneIntersectionTest, TestingWOrldSceneIntersection)
     sp1.set_material(mat1);
 
     shapes::Sphere sp2{Vector<float>{0, 0, 0}, 1.0, 1};
-    std::cout << sp2.get_transform() << std::endl;
     sp2.scale(Vector<float>{0.5, 0.5, 0.5});
-    std::cout << sp2.get_radius() << std::endl;
-    std::cout << sp2.get_transform() << std::endl;
 
     WorldScene world_scene{};
     world_scene.add_shape(sp1);
     world_scene.add_shape(sp2);
 
     Ray ray{Vector<float>{0, 0, -5}, Vector<float>{0, 0, 1}};
-
     std::vector<types::intersection> intersections = world_scene.intersect(ray);
-
-    for(auto inter : intersections)
-        std::cout << inter.t << std::endl;
 
     ASSERT_EQ(intersections.size(), 4);
     ASSERT_EQ(intersections[0].t, 4);
     ASSERT_EQ(intersections[1].t, 4.5);
     ASSERT_EQ(intersections[2].t, 5.5);
     ASSERT_EQ(intersections[3].t, 6);
+}
+
+TEST(WorldScenePrecompIntersectionStateTest, TestingWorldSceneIntersectionStatePrecomp)
+{
+    Ray ray{Vector<float>{0, 0, -5}, Vector<float>{0, 0, 1}};
+    shapes::Sphere sp{Vector<float>{0, 0, 0}, 1.0, 0};
+    types::intersection intersection;
+    intersection.obj_type = sp.get_type();
+    intersection.t = 4;
+
+    WorldScene world_scene{};
+    types::intersection_state intersection_state = world_scene.precompute_intersection_state(&sp, intersection, ray);
+
+    Vector<float> point_ref{0, 0, -1};
+    Vector<float> eye_dir_ref{0, 0, -1};
+    Vector<float> normal_ref{0, 0, -1};
+
+    ASSERT_EQ(intersection_state.t, intersection.t);
+    ASSERT_EQ(intersection_state.obj_type, intersection.obj_type);
+    ASSERT_EQ(intersection_state.point, point_ref);
+    ASSERT_EQ(intersection_state.eye_dir, eye_dir_ref);
+    ASSERT_EQ(intersection_state.normal, normal_ref);
 }
 
 
