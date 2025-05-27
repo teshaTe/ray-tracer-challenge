14c422f329c479a07de119cd67b80fdecc267100
teshaTe <tereshin.tesha@gmail.com>
2025-05-27 21:04:21 +0100

    added shading for multiple objects in the scene

--- RayTracer/Core/worldscene.cpp
+++ RayTracer/Core/worldscene.cpp
@@ -2,6 +2,8 @@
 #include <memory>
 
 #include "worldscene.h"
+#include "Lights/point_light.h"
+#include "Shapes/sphere.h"
 
 
 namespace ray_tracer {
@@ -21,6 +23,24 @@ void WorldScene::print_scene_objects()
     }
 }
 
+void WorldScene::delete_shape(const int &id, const std::string shape_type)
+{
+    m_shapes.erase(std::remove_if(m_shapes.begin(), m_shapes.end(),
+                                  [&](const std::unique_ptr<shapes::Shape>& s) {
+                                      return (s.get()->get_id()==id && s.get()->get_type()==shape_type);
+                                  }
+                                  ), m_shapes.end());
+}
+
+void WorldScene::delete_light(const int &id, const std::string light_type)
+{
+    m_lights.erase(std::remove_if(m_lights.begin(), m_lights.end(),
+                                  [&](const std::unique_ptr<lights::Light>& l) {
+                                      return (l.get()->get_id()==id && l.get()->get_type()==light_type);
+                                  }
+                                  ), m_lights.end());
+}
+
 std::vector<types::intersection> WorldScene::intersect(const Ray &ray)
 {
     std::vector<types::intersection> all_intersections;
@@ -33,16 +53,54 @@ std::vector<types::intersection> WorldScene::intersect(const Ray &ray)
     return all_intersections;
 }
 
-types::intersection_state WorldScene::precompute_intersection_state(shapes::Shape *shape, types::intersection &intersection, Ray &ray)
+types::intersection_state WorldScene::precompute_intersection_state(shapes::Shape &shape, types::intersection &intersection, Ray &ray)
 {
+    Vector<float> point = ray.position(intersection.t);
+    Vector<float> eye_dir = ray.get_direction().negate();
+    Vector<float> normal = shape.get_normal(point);
+
+    bool inside = false;
+    if(normal.dot(eye_dir) < 0)
+    {
+        inside = true;
+        normal = normal.negate();
+    }
+
     types::intersection_state state{
         intersection.obj_type,
-        ray.position(intersection.t),
-        ray.get_direction().negate(),
-        shape->get_normal(state.point),
+        intersection.obj_id,
+        point,
+        eye_dir,
+        normal,
         intersection.t,
+        inside
     };
     return state;
+}
+
+void WorldScene::create_default_world()
+{
+    materials::BaseMaterial mat1;
+    mat1.color = Color<float>{0.8, 1.0, 0.6};
+    mat1.ambient = 0.1;
+    mat1.diffuse = 0.7;
+    mat1.specular = 0.2;
+    mat1.shininess = 200.0;
+
+    Ray ray1{Vector<float>{0, 0, -5}, Vector<float>{0, 0, 1}};
+    shapes::Sphere sp1{Vector<float>{0, 0, 0}, 1, 0};
+    sp1.set_material(mat1);
+
+    shapes::Sphere sp2{Vector<float>{0.0, 0.0, 0.0}, 1.0, 1};
+    sp2.scale(Vector<float>{0.5, 0.5, 0.5});
+    materials::BaseMaterial mat2;
+    sp2.set_material(mat2);
+
+    add_shape(sp1);
+    add_shape(sp2);
+
+    lights::PointLight plight1{Vector<float>{-10, 10, -10}, Color<float>{1, 1, 1}, 0};
+    add_light(plight1);
 }
 
 } // namespace ray_tracer
