2e707a5dab1250281ae9d9f2e7470db809c7f19c
teshaTe <tereshin.tesha@gmail.com>
2025-05-27 20:59:58 +0100

    added shading comp. for multiple objects

--- RayTracer/Core/RayTracer.cpp
+++ RayTracer/Core/RayTracer.cpp
@@ -5,7 +5,7 @@
 namespace ray_tracer {
 
 Color<float> RayTracer::compute_lightning(materials::BaseMaterial &material,
-                                          lights::PointLight &light,
+                                          lights::Light &light,
                                           Vector<float> &point,
                                           Vector<float> &eye_dir,
                                           Vector<float> &normal)
@@ -45,12 +45,43 @@ Color<float> RayTracer::compute_lightning(materials::BaseMaterial &material,
             specular = black;
         } else {
             // compute the specular contribution
-            float factor = std::pow(reflect_dot_eye, material.snininess);
+            float factor = std::pow(reflect_dot_eye, material.shininess);
             specular = light.get_intensity() * material.specular * factor;
         }
     }
 
     return ambient + diffuse + specular;
+}
+
+Color<float> RayTracer::compute_shading(WorldScene &world_scene, types::intersection_state &intersection_state)
+{
+    const auto& shapes = world_scene.get_shapes();
+    const auto& lights = world_scene.get_lights();
+
+    std::unique_ptr<shapes::Shape> cur_shape;
+    for(int i=0; i < shapes.size(); i++)
+    {
+        if(shapes[i]->get_id() == intersection_state.obj_id && shapes[i]->get_type() == intersection_state.obj_type)
+        {
+            cur_shape = shapes[i]->clone();
+            break;
+        }
+    }
+
+    Color<float> final_color{0, 0, 0};
+    for(int j=0; j < lights.size(); j++)
+    {
+        materials::BaseMaterial material = cur_shape.get()->get_assigned_material();
+        Color<float> color = compute_lightning(material,
+                                               *lights[j],
+                                               intersection_state.point,
+                                               intersection_state.eye_dir,
+                                               intersection_state.normal);
+        final_color = final_color + color;
+    }
+
+    return final_color;
+
 }
 
 
--- RayTracer/Core/RayTracer.h
+++ RayTracer/Core/RayTracer.h
@@ -4,6 +4,9 @@
 #include "Material.hpp"
 #include "Lights/point_light.h"
 #include "Containers/Vector.hpp"
+#include "Containers/Color.hpp"
+#include "DataTypes.hpp"
+#include "worldscene.h"
 
 
 namespace ray_tracer {
@@ -16,10 +19,12 @@ public:
     RayTracer() = default;
 
     Color<float> compute_lightning(materials::BaseMaterial &material,
-                                   lights::PointLight &light,
+                                   lights::Light &light,
                                    Vector<float> &point,
                                    Vector<float> &eye_dir,
                                    Vector<float> &normal);
+
+    Color<float> compute_shading(WorldScene &world_scene, types::intersection_state &intersection_state);
 
 
     ~RayTracer() = default;
