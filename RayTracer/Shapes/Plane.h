79abfea7ca2a6e3e1d2d82a85157b4aed4eba19e
teshate <tereshin.tesha@gmail.com>
2026-04-19 19:58:05 +0100

    Added Plane primitive impl.

---
+++ RayTracer/Shapes/Plane.h
@@ -0,0 +1,66 @@
+#ifndef PLANE_H
+#define PLANE_H
+
+#include <memory>
+#include <string>
+
+#include "Containers/Vector.hpp"
+#include "Core/Shape.h"
+#include "Core/DataTypes.hpp"
+#include "Core/Ray.hpp"
+#include "Core/Material.hpp"
+#include "Core/MatrixUtils.hpp"
+
+
+namespace ray_tracer::shapes {
+
+class Plane : public Shape
+{
+private:
+    Matrix<float> m_transform_mat;
+    Vector<float> m_origin;
+    ray_tracer::materials::BaseMaterial m_material;
+    std::string m_obj_name;
+    int m_id;
+    std::string m_type = "plane";
+    MatrixUtlities m_mat_utils{};
+
+private:
+    std::vector<types::intersection> m_local_intersect(Ray &ray);
+
+public:
+    Plane(const int id);
+
+    /**
+     * @brief The descritption for functions below can be found in shape.h file.
+     *        These methods override methods from the parent class.
+     */
+    // functions for changing/getting sphere transformation matrix/data
+    void transform(Matrix<float> &transform) override;
+
+    std::string get_type() const override { return m_type; }
+    Matrix<float> get_transform() override { return m_transform_mat; }
+    Matrix<float> get_rotation_matrix() override { return m_transform_mat.block(3, 3, 0, 0); }
+    Vector<float> get_translation() override { return m_transform_mat.block(3, 1, 0, 3).to_vec_1x3(); }
+    Vector<float> get_normal(const Vector<float> &point) override;
+    Vector<float> get_origin() const override { return m_origin; }
+    int get_id() const override { return m_id; }
+
+    // scale the object up or down
+    void scale(const Vector<float> &scale_vec) override;
+
+    void set_material(ray_tracer::materials::BaseMaterial &material) override;
+    ray_tracer::materials::BaseMaterial get_assigned_material() const override { return m_material; };
+
+    // compute the intersection of the rays with the sphere
+    std::vector<types::intersection> intersect(Ray &ray) override;
+
+    std::unique_ptr<shapes::Shape> clone() const override {
+        return std::make_unique<Plane>(*this); // Copy itself
+    }
+
+    ~Plane() = default;
+};
+
+} // namespace ray_tracer::shapes
+#endif // PLANE_H
