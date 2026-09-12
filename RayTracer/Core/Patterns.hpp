1ff8d880350b9f32bece4ade495e15c2c89014ec
teshate <tereshin.tesha@gmail.com>
2026-09-12 18:15:12 +0100

    added function for colouring the object in its local space

--- RayTracer/Core/Patterns.hpp
+++ RayTracer/Core/Patterns.hpp
@@ -3,7 +3,7 @@
 
 #include "Containers/Color.hpp"
 #include "Containers/Vector.hpp"
-
+#include "Containers/Matrix.hpp"
 
 namespace ray_tracer{
 
@@ -11,8 +11,22 @@ struct BasePattern
 {
     Color<float> color1;
     Color<float> color2;
-    BasePattern(const Color<float>& c1, const Color<float>& c2): color1(c1), color2(c2) {}
+    Matrix<float> pattern_tr{4, 4};
+
+    BasePattern(const Color<float>& c1, const Color<float>& c2): color1(c1), color2(c2) { pattern_tr.I(); }
     virtual Color<float> pattern_at(const Vector<float> &point) = 0;
+    Color<float> pattern_at_object(Matrix<float> &shape_tr, const Vector<float> &point)
+    {
+        Vector<float> obj_point = shape_tr.inv().mul(point).to_vec_1x4();
+        Vector<float> pattern_point = pattern_tr.inv().mul(obj_point).to_vec_1x3();
+        return pattern_at(pattern_point);
+    }
+
+    void set_pattern_transform(const Matrix<float> &transform)
+    {
+        pattern_tr = transform;
+    }
+
     virtual ~BasePattern() = default;
 };
 
@@ -23,8 +37,8 @@ struct StripePattern: BasePattern
 
     Color<float> pattern_at(const Vector<float> &point)
     {
-        int n = std::floor(point[0]);
-        if(n % 2 == 0 )
+        int n = static_cast<int>(std::floor(point[0]));
+        if(n % 2 == 0)
             return color1;
         else
             return color2;
